c8359e05ec5719a769574cb4d7944dd9f4aa2b42
Ryuhyis <skyblue22756@gmail.com>
2023-08-10 00:30:11 +0900

    feat: 로또 게임 실행

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,22 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.controller.LottoController;
+import lotto.domain.Lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        try {
+            LottoController lottoController = new LottoController();
+            lottoController.playGame();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e);
+        }
+
     }
 }

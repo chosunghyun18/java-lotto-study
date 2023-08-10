279ff21e551fb06e38b95fcecda5f1cae48c9999
Brian <bwook9908@gmail.com>
2023-08-10 14:45:07 +0900

    feat: lotto generator created

---
+++ src/main/java/lotto/utils/LottoGenerator.java
@@ -0,0 +1,10 @@
+package lotto.utils;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.model.Lotto;
+
+public class LottoGenerator {
+    public static Lotto generateLotto() {
+        return new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+    }
+}
